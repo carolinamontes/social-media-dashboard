4ca20b08a05f352a5e3ce7ea543e01a622edd215
Carolina Montes <153744526+carolinamontes@users.noreply.github.com>
2024-03-03 12:00:47 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+const App = () => {
+	return (
+		<>
+		<GlobalStyles/>
+		<h1>Nucleo de la aplicacion</h1>
+		</>
+	)
+};
+
+export default App;
